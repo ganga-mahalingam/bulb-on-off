22182fab8562942f951037fd4f1ab74585630aad
Ganga Mahalingam <gangamahalingam19@gmail.com>
2025-03-19 16:03:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// var,let,const -TYPES OF DECLARING VARIABLE
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlpf9tcAGq0rjYcgVV59mlVw47ViWNLmuEgw&s"
+}
+
+function bulb_off()
+{
+    img.src="https://as1.ftcdn.net/jpg/01/57/61/52/1000_F_157615259_sJofv2nBJDjoIJBduVYunJrGwjWOUUiD.jpg"
+
+}
